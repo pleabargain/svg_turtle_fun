271b94adc42aeb99cbb94b911358e1855bf675e5
Dennis <dennisgdaniels@gmail.com>
2022-11-23 18:01:18 +0400

    thank you Horst!

--- geometric_turtle_SVG_drawingSVG.py
+++ geometric_turtle_SVG_drawingSVG.py
@@ -9,9 +9,20 @@ import datetime
 time_stamp = (str(datetime.datetime.now().hour) + "_" + str(datetime.datetime.now().minute))
 
 from svg_turtle import SvgTurtle
+# create a variable the includes all functions
 
 #set the size of the turtle screen
-t = SvgTurtle(700, 700)
+# uppercase
+screenheighty = 5000
+screenwidthx = 5000
+t = SvgTurtle(screenwidthx, screenheighty, )
+
+rows = 5
+cols = 5
+
+width = screenwidthx / cols
+height = screenheighty / rows
+
 
 # function draw a star
 def draw_star():
@@ -21,9 +32,13 @@ def draw_star():
 
 # create a function that draws a hexagon
 def draw_hexagon():
+    t.fillcolor('red')
+    t.begin_fill()
     for i in range(6):
         t.forward(100)
         t.right(60)
+        # t.write("hello")
+    t.end_fill()
 
 
 
@@ -49,6 +64,18 @@ def find_center():
     t.goto(0,0)
     t.pendown()
 
+# create a spiral function that takes a parameter
+def draw_rotation(range_variable = 60, figure = draw_square):
+    #make sure the spiral covers 360 degrees
+    # note the higher the range_variable, the fewer the squares
+    for i in range(int(360/range_variable)):
+        # draw_square()
+        figure()
+        t.right(range_variable)
+
+
+
+
 # create a function that draws a spiral of squares
 def draw_spiral_of_squares():
     #make sure the spiral covers 360 degrees
@@ -72,27 +99,55 @@ def draw_benzene_spiral():
         t.left(59)
 
 
-# create a variable the includes all functions
-all_functions = [find_center,
-                draw_benzene_spiral,
-                # draw_spiral_of_squares,
+
+# # create a for loop that runs through all functions
+# for i in all_functions:
+#     i()
+
+# find_center()
+# draw_rotation(60, draw_star)
+all_functions = [#  draw_benzene_spiral,
+                #  draw_spiral_of_squares,
+                # draw_rotation,
                 # draw_square, 
+                #  find_center,
+                 draw_circle, 
                 # find_center,
-                # draw_circle, 
+                draw_star,
                 # find_center,
-                # draw_star,
+                 draw_triangle,
                 # find_center,
-                # draw_triangle,
-                # find_center,
-                # draw_hexagon,
-                
+                 draw_hexagon,
                 ]
 
-# create a for loop that runs through all functions
-for i in all_functions:
-    i()
 
 # save the drawing
+# go to lower left corner cell
+i = 0
+t.penup()
+t.goto(-screenwidthx/2, screenheighty/2)
+for y in range(rows):
+    for x in range(cols):
+        t.pensize(5)
+        t.penup()
+        t.goto(-screenwidthx/2 + x*width,screenheighty/2 - y*height)
+        t.pendown()
+        #now at top lop left of cell
+        #draw a square
+        for _ in range(2):
+            t.forward(width)
+            t.right(90)
+            t.forward(height)
+            t.right(90)
+        # center of cell
+        t.penup()
+        t.goto(-screenwidthx/2 + x*width + width/2,screenheighty/2 - y*height - height/2)
+        t.pendown()
+        # t.set_heading(0)
+        func = all_functions[i%len(all_functions) ]
+        draw_rotation(60, func)
+        i += 1
+        
 t.save_as(time_stamp + 'circle_square_example.svg')
 
 #run the function
